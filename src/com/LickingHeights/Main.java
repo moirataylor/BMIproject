2d1092aa75ee7237b1584efd8e28d0be882d1d25
moira <moira.taylor.2023@student.lhschools.org>
2019-09-17 09:44:58 -0400

    greeting user

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,10 +1,23 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+       Scanner Keyboard;
+        Keyboard = new Scanner(System.in);
 
-        
+        System.out.println("Hello!");
+
+
+
+
+
+
+
+
+
 
 
 
