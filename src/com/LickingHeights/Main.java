41c4358aaeadcc165031f95b9e0da8b824c43122
moira <moira.taylor.2023@student.lhschools.org>
2019-09-17 09:50:51 -0400

    asking for the users height

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,12 +7,19 @@ public class Main {
     public static void main(String[] args) {
        Scanner Keyboard;
        String whatsYourName;
+       int height,weight;
+
+
         Keyboard = new Scanner(System.in);
 
         System.out.println("Hello!");
         System.out.println("Today we're going to be finding your BMI.");
         System.out.println("what's your name?");
         whatsYourName = Keyboard.nextLine();
+        System.out.print("good to know! first question, how tall are you in inches?");
+        height = Keyboard.nextInt();
+
+
 
 
 
