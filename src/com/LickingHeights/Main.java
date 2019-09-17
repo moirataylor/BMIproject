6e5cc6ac8d4723979a3db731320b3426b7996dd1
moira <moira.taylor.2023@student.lhschools.org>
2019-09-17 09:46:54 -0400

    asking user for their name

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,9 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
        Scanner Keyboard;
+       String whatsYourName;
         Keyboard = new Scanner(System.in);
 
         System.out.println("Hello!");
+        System.out.println("Today we're going to be finding your BMI.");
+        System.out.println("what's your name?");
+        whatsYourName = Keyboard.nextLine();
 
 
 
